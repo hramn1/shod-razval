dd1dbad283b878a949795eaf22681b3150408cc7
Stas <stmihailov@yandex.ru>
2017-08-31 01:24:15 +0300

    правки

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  var el = document.querySelector(".main-header");
+  window.addEventListener("scroll", function(event){
+    if (window.pageYOffset > 200)
+      el.classList.add("head-fix");
+    else
+      el.classList.remove("head-fix");
+  });
+}
+  
